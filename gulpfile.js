4ad8e66dbe91781b4f9207db9ccce34edb08bc27
Charles Da Silva Costa <CDASILVA@3KLES.local>
2024-02-02 20:32:44 +0100

    Fix gulp publish

--- gulpfile.js
+++ gulpfile.js
@@ -126,12 +126,13 @@ gulp.task('clean-build', (done) => {
 
 // PUBLISH
 gulp.task('publish', gulp.series('transpile', (cb) => {
-	exec('npm publish --force', function (err, stdout, stderr) {
+	exec('npm publish --force --access public', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
 		cb(err);
 	});
 }));
+
 
 
 // WATCH
